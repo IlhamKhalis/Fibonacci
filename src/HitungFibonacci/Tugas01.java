dbec08f99e783ed6beb37a9c124c53b299b3f03b
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-13 13:00:25 +0700

    Initial Commit

---
+++ src/HitungFibonacci/Tugas01.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonacci;
+
+/**
+ *
+ * @author Ilham
+ */
+public class Tugas01 {
+    public static void main(String[] args) {
+        String identitas = "Ilham Khalis Putra Agatha / X RPL 2 / 17";
+        
+        tampilJudul(identitas);
+    }
+    public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+}
