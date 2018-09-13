ed16cbd21cb21c7ae5153c23e56e858cacd793c5
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-13 14:06:14 +0700

    Initial Commit

---
+++ src/HitungFibonacci/Praktek01.java
@@ -0,0 +1,18 @@
+
+package HitungFibonacci;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Praktek01 {
+    
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+}
+
+
