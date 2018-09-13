344484c870e441b927db2c737ccd56db9969087f
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-13 13:05:47 +0700

    Tambah Input

--- src/HitungFibonacci/Tugas01.java
+++ src/HitungFibonacci/Tugas01.java
@@ -4,13 +4,14 @@
  * and open the template in the editor.
  */
 package HitungFibonacci;
-
+import java.util.Scanner;
 /**
  *
  * @author Ilham
  */
 public class Tugas01 {
     public static void main(String[] args) {
+        int n = tampilInput();
         String identitas = "Ilham Khalis Putra Agatha / X RPL 2 / 17";
         
         tampilJudul(identitas);
@@ -20,5 +21,13 @@ public class Tugas01 {
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    public static int tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = scanner.nextInt();
+        
+        return n;
     }
 }
