92d0c5f6760c300f40c06d73cd486bfe236137f0
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-13 14:08:48 +0700

    Tambah Tampil Hasil

--- src/HitungFibonacci/Praktek01.java
+++ src/HitungFibonacci/Praktek01.java
@@ -32,5 +32,7 @@ public class Praktek01 {
         
         return hasil[n-1];
     }
-    
+    private static void tampilHasil(int n, BigInteger hasil) {
+        System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
+    }
 }
