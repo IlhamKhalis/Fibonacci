5510102e7dc30a8b2c4c1ef3a363b1ef0d3e29a3
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-14 20:40:11 +0700

    Initial Commit

---
+++ src/HitungFibonacci/Tugas02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HitungFibonacci;
+import java.util.Scanner;
+/**
+ *
+ * @author Ilham
+ */
+public class Tugas02 {
+    public static void main(String[] args) {
+        Scanner sistem = new Scanner(System.in);
+        String identitas = "Ilham Khalis Putra Agatha / X RPL 2 / 17";
+        int arr[]= {7, 5, 4, 3, 2};
+        int arr1[]= {10000, 7000, 5000, 3000, 2000};
+        int bayaran = 0;
+        
+        
+    }
+}
