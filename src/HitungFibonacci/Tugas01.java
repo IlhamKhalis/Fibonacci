fd95ed2d89562e86c8d7e1606640d54044f1444c
Ilham <Ilham@LAPTOP-0BO61VTL.mshome.net>
2018-09-14 20:38:06 +0700

    Initial Commit

--- src/HitungFibonacci/Tugas01.java
+++ src/HitungFibonacci/Tugas01.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package HitungFibonacci;
-import java.math.BigInteger;
 import java.util.Scanner;
 /**
  *
@@ -12,40 +11,10 @@ import java.util.Scanner;
  */
 public class Tugas01 {
     public static void main(String[] args) {
-        int n = tampilInput();
-        BigInteger hasil = fibo(n);
-        tampilHasil(n, hasil);
         String identitas = "Ilham Khalis Putra Agatha / X RPL 2 / 17";
+        int [] nominal =  {3000, 2000, 500, 200, 100};
+        Scanner sistem = new Scanner(System.in);
         
-        tampilJudul(identitas);
-    }
-    private static void tampilJudul(String identitas) {
-        System.out.println("Identitas : " + identitas);
         
-        System.out.println("\nHitung Fibonacci");
-        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
-    }
-    private static int tampilInput() {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Bilangan ke-: ");
-        int n = scanner.nextInt();
-        
-        return n;
-    }
-    private static BigInteger fibo(int n) {
-        BigInteger[] hasil = new BigInteger[n];
-        
-        hasil[0] = BigInteger.ONE;
-        hasil[1] = BigInteger.ONE;
-        
-        for (int i = 2; i < n; i++) {
-            hasil[i] = hasil[i-1].add(hasil[i-2]);     
-        }
-        
-        return hasil[n-1];
-    }
-    private static void tampilHasil(int n, BigInteger hasil) {
-        System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
     }
 }
